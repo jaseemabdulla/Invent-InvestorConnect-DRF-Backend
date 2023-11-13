3fd0c00fd6526a2b72121218673e36bf1572106e
jaseem <jaseempk003@gmail.com>
2023-11-13 23:29:44 +0530

    api for refresh token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginApi,SignUpApi
+from .views import LoginApi,SignUpApi,RefreshTokenView
 
 urlpatterns = [
     path('login/',LoginApi.as_view()),
-    path('signup/',SignUpApi.as_view())
+    path('signup/',SignUpApi.as_view()),
+    path('refreshToken/',RefreshTokenView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+import jwt
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # Create your views here.
 
@@ -12,6 +15,7 @@ from rest_framework import status
 class LoginApi(APIView):
     
     def post(self, request):
+       
         try:
             data = request.data
             serializer = LoginSerializer(data= data)
@@ -38,10 +42,14 @@ class LoginApi(APIView):
                 serialized = SignupSerializer(instance=user)
                 serialized_user = serialized.data 
                 refresh = RefreshToken.for_user(user)
-                data = {
+                token = {
                     'refresh': str(refresh),
-                    'access': str(refresh.access_token),
-                    'user': serialized_user
+                    'access': str(refresh.access_token)
+                }
+                data = {
+                    'token': token,
+                    'user': serialized_user,
+                    'message': 'login succesfully'
                 }
 
                 return Response(data,status=status.HTTP_200_OK)    
@@ -73,6 +81,50 @@ class SignUpApi(APIView):
             }
                 
         return Response(data,status=status.HTTP_400_BAD_REQUEST)
+    
+class RefreshTokenView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh',None)
+            if not refresh_token:
+                return Response({'error':'No valid refresh token'},status=status.HTTP_401_UNAUTHORIZED)
+            
+            try:
+                refresh = RefreshToken(refresh_token)
+                
+                # if no exeption is raised its a valid refrsh token
+            except jwt.ExpiredSignatureError:  
+                return Response({'error':'Refresh token has expierd.'},status=status.HTTP_401_UNAUTHORIZED)
+            except jwt.InvalidTokenError:
+                return Response({'error':'Invalid refresh token.'},status=status.HTTP_401_UNAUTHORIZED)
+            
+            decoded_payload = jwt.decode(refresh_token, key=settings.SECRET_KEY, algorithms=['HS256'])  
+            
+            user_id = decoded_payload.get('user_id')
+            
+            user = get_user_model().objects.get(id = user_id)
+            
+            new_refresh = RefreshToken.for_user(user)
+            
+            token = {
+                    'refresh': str(new_refresh),
+                    'access': str(new_refresh.access_token)
+                }
+            
+            data = {
+                    'token': token,
+                }
+            
+            return Response(data,status=status.HTTP_200_OK)
+        
+        except get_user_model().DoesNotExist:
+            return Response({'error':'User assosiated with this token does not exist.'},status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as e:
+            return Response({'error':f'error:{e}'},status=status.HTTP_400_BAD_REQUEST)
+            
+            
+  
             
         
             

1134f6602ec1e4c0f1879b7e8341ec9cfcac8296
jaseem <jaseempk003@gmail.com>
2024-01-03 17:26:09 +0530

    fix: changed daphne settings

--- backend_invent/asgi.py
+++ backend_invent/asgi.py
@@ -4,9 +4,14 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from . import routing
+
+from django import setup
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_invent.settings')
+setup()
+
+
+from . import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
